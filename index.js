10a56f0289aa52659ce6298f4d4fa1eb9fdc37bb
muizece <muiz.ece@gmail.com>
2023-01-28 19:56:22 +0530

    Refactored and added user.jason to update MongoDB

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
-import {options} from './options.js';
+import { options } from './options.js';
 import multer from 'multer';
 import path from 'path';
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mbsoftrecruitdb', { 
+mongoose.connect('mongodb://127.0.0.1:27017/mbsoftrecruitdb', {
     useNewUrlparser: true,
     useunifiedTopology: true
 }, () => {
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     id: Number,
-    role:String,
+    role: String,
 });
 
 
@@ -35,26 +35,23 @@ const Users = new mongoose.model("User", userSchema);
 
 
 // View Engine Setup
-// app.set("views",path.join(__dirname,"views"))
-
-app.set("views","views")
-app.set("view engine","ejs")
-
+app.set("views", path.join("views"))
+app.set("view engine", "ejs")
 
 //Application Routes - Start
 app.post("/login", (req, res) => {
     const { uid, pd } = req.query;
-    Users.findOne({name: uid, password: pd}, (err, user) => {
-        if(err) {
+    Users.findOne({ name: uid, password: pd }, (err, user) => {
+        if (err) {
             res.status(500).json({
                 message: 'Error'
-                });
+            });
         }
-        if(user) {
+        if (user) {
             let userdata = {
                 username: uid,
                 password: pd,
-                role:user.role
+                role: user.role
             }
             let token = jwt.sign(userdata, options.skey, {
                 algorithm: 'HS256',
@@ -64,59 +61,56 @@ app.post("/login", (req, res) => {
             res.status(200).json({
                 message: 'Login Successful',
                 jwtoken: 'Bearer ' + token
-                });
+            });
 
-        }else {
+        } else {
             res.status(401).json({
                 message: 'Login Failed 2'
-                });
+            });
         }
     });
 });
 
 app.post("/register", (req, res) => {
     console.log(req.query);
-    const { name, email, password} = req.query;
+    const { name, email, password } = req.query;
 
-    Users.findOne({email: email}, (err, user) => {
-        if(user) {
-            res.send({message: "User already exists"})
+    Users.findOne({ email: email }, (err, user) => {
+        if (user) {
+            res.send({ message: "User already exists" })
         } else {
             const user = new Users({
                 name, email, password
             });
 
             user.save(err => {
-                if(err){
+                if (err) {
                     res.send(err)
                 } else {
-                    res.send({message: "User registered scuccessfully!!"})
+                    res.send({ message: "User registered scuccessfully!!" })
                 }
             });
         }
     });
 });
 
-
-
-
 //Application Routes - End
 
-
 // Test Routes - START
+
 app.post("/testtoken", verifyToken, (req, res, next) => {
     jwt.verify(req.token, options.skey, (err, data) => {
-        if(err){
+        if (err) {
             res.status(403).json({
-                message: "Invalid Token !!",        
-              });
+                message: "Invalid Token !!",
+            });
         } else {
 
             res.status(200).json({
-                message: "Token validated 11 !!", 
-                addFn2: addFn(1,20)       
-              });
-        }            
+                message: "Token validated 11 !!",
+                addFn2: addFn(1, 20)
+            });
+        }
     })
 });
 
@@ -124,88 +118,84 @@ app.get("/test", (req, res) => {
     res.send("test");
 });
 
-
-
 // update pd using name
 app.post("/uppd", (req, res) => {
-    const { name, pd} = req.query;
-    Users.updateOne({name: name}, { $set : { password : pd }}, (err, data) => {
-        if(err){
+    const { name, pd } = req.query;
+    Users.updateOne({ name: name }, { $set: { password: pd } }, (err, data) => {
+        if (err) {
             res.status(403).json({
-                message: "NOT Updated !!",        
-              });
+                message: "NOT Updated !!",
+            });
         } else {
             res.status(200).json({
-                updtedData: data     
-              });
-        }  
+                updtedData: data
+            });
+        }
     });
 });
-
-
 
 // Test Routes - END
 
 // var upload = multer({ dest: "Upload_folder_name" })
 // If you do not want to use diskStorage then uncomment it
-    
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-  
+
         // Uploads is the Upload_folder_name
         cb(null, "uploads")
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now()+".jpg")
+        cb(null, file.fieldname + "-" + Date.now() + ".jpg")
     }
-  })
-       
+})
+
 // Define the maximum size for uploading
 // picture i.e. 1 MB. it is optional
 const maxSize = 3 * 1000 * 1000;
-    
-var upload = multer({ 
+
+var upload = multer({
     storage: storage,
     limits: { fileSize: maxSize },
-    fileFilter: function (req, file, cb){
-    
+    fileFilter: function (req, file, cb) {
+
         // Set the filetypes, it is optional
         var filetypes = /jpeg|jpg|png/;
         var mimetype = filetypes.test(file.mimetype);
-  
+
         var extname = filetypes.test(path.extname(
-                    file.originalname).toLowerCase());
-        
+            file.originalname).toLowerCase());
+
         if (mimetype && extname) {
             return cb(null, true);
         }
-      
+
         cb("Error: File upload only supports the "
-                + "following filetypes - " + filetypes);
-      } 
-  
-// mypic is the name of file attribute
-}).single("mypic");       
-  
-app.get("/",function(req,res){
+            + "following filetypes - " + filetypes);
+    }
+
+    // mypic is the name of file attribute
+}).single("mypic");
+
+app.get("/", function (req, res) {
     res.render("Signup");
 })
-    
-app.post("/uploadProfilePicture",function (req, res, next) {
-        
+
+app.post("/uploadProfilePicture", function (req, res, next) {
+
     // Error MiddleWare for multer file upload, so if any
     // error occurs, the image would not be uploaded!
-    upload(req,res,function(err) {
-  
-        if(err) {
-  
+    upload(req, res, function (err) {
+
+        if (err) {
+
             // ERROR occurred (here it can be occurred due
             // to uploading image of size greater than
             // 1MB or uploading different file type)
             res.send(err)
         }
         else {
-  
+
             // SUCCESS, image successfully uploaded
             res.send("Success, Image uploaded!")
         }
@@ -216,38 +206,24 @@ app.post("/uploadProfilePicture",function (req, res, next) {
 function verifyToken(req, res, next) {
 
     const bearerHeader = req.headers["authorization"];
-  
+
     if (typeof bearerHeader !== "undefined") {
-  
-      const bearerToken = bearerHeader.split(" ")[1];
-  
-      req.token = bearerToken;
-  
-      next();
-  
+
+        const bearerToken = bearerHeader.split(" ")[1];
+
+        req.token = bearerToken;
+
+        next();
+
     } else {
-  
-      res.sendStatus(403);
-  
+
+        res.sendStatus(403);
+
     }
-  
+
 }
-
-
 
 app.listen(9600, () => {
     console.log("hi from Node from 9600!!")
 });
 
-/*
-var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
-var url = "mongodb://localhost:27017/mydb";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-
-*/
